9722b82edaecb40d331499439ea3ddb7c1d0442b
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-23 17:58:38 +0330

    release: bump version to 1.3.3 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.marketrates.irgold"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10
-    versionName = "1.3.2"
+    versionCode = 11
+    versionName = "1.3.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
